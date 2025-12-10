540033e0b3e76e0fa074bf9a280d838364014db0
Aviral <aviralutkarsh87@gmail.com>
2025-12-10 17:27:41 +0530

    Changed requirements

--- app.py
+++ app.py
@@ -79,6 +79,10 @@ def recommend():
 
     return jsonify({"recommendations": ranked})
 
+import os
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
+
